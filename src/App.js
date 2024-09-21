28aee5aaed7be360a1b5870ed93933d9667edc17
Aiden Cullo <aidencullo@gmail.com>
2024-09-21 18:55:28 -0400

    useEffect on roomId change

--- src/App.js
+++ src/App.js
@@ -1,13 +1,37 @@
-import { useEffect } from 'react';
-import { createConnection } from './chat.js';
+import { useState, useEffect } from "react";
+import { createConnection } from "./chat.js";
 
-export default function ChatRoom() {
+const serverUrl = "https://localhost:1234";
+
+function ChatRoom({ roomId }) {
   useEffect(() => {
-    const connection = createConnection();
+    const connection = createConnection(serverUrl, roomId);
     connection.connect();
-    return () => {
-      connection.disconnect();
-    };
-  }, []);
-  return <h1>Welcome to the chat!</h1>;
+    return () => connection.disconnect();
+  }, [roomId]);
+
+  return <h1>Welcome to the {roomId} room!</h1>;
+}
+
+export default function App() {
+  const [roomId, setRoomId] = useState("general");
+  const [show, setShow] = useState(false);
+  return (
+    <div className="centered">
+      <label>
+        <b>Choose a Chat Room</b>
+        <br />
+        <select value={roomId} onChange={(e) => setRoomId(e.target.value)}>
+          <option value="general">General</option>
+          <option value="travel">Travel</option>
+          <option value="music">Music</option>
+        </select>
+      </label>
+      <button onClick={() => setShow(!show)}>
+        {show ? "Close Chat" : "Open Chat"}
+      </button>
+      <br />
+      {show && <ChatRoom roomId={roomId} />}
+    </div>
+  );
 }
--- src/chat.js
+++ src/chat.js
@@ -1,11 +1,11 @@
-export function createConnection() {
+export function createConnection(serverUrl, roomId) {
   // A real implementation would actually connect to the server
   return {
     connect() {
-      console.log("✅ Connecting...");
+      console.log('✅ Connecting to "' + roomId + '" room at ' + serverUrl + '...');
     },
     disconnect() {
-      console.log("❌ Disconnected.");
+      console.log('❌ Disconnected from "' + roomId + '" room at ' + serverUrl);
     }
   };
 }
