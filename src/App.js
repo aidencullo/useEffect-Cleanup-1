e0a54a574df01d71298d2773b9145d39bbc68b6e
Aiden Cullo <aidencullo@gmail.com>
2024-09-23 16:23:30 -0400

    add scroll event listener to window object

--- src/App.js
+++ src/App.js
@@ -1,21 +1,22 @@
-import Box from './Box.js';
+import { useEffect } from 'react';
 
 export default function App() {
+
+  useEffect(() => {
+    function handleScroll(e) {
+      console.log(window.scrollX, window.scrollY);
+    }
+    window.addEventListener('scroll', handleScroll);
+    return () => window.removeEventListener('scroll', handleScroll);
+  }, []);
+  
   return (
     <>
-      <LongSection />
-      <Box />
-      <LongSection />
-      <Box />
-      <LongSection />
+      {[...Array(100)].map((_, i) => (
+	<div key={i} style={{ height: '100vh' }}>
+	  {i}
+	</div>
+      ))}
     </>
   );
-}
-
-function LongSection() {
-  const items = [];
-  for (let i = 0; i < 50; i++) {
-    items.push(<li key={i}>Item #{i} (keep scrolling)</li>);
-  }
-  return <ul>{items}</ul>
 }
