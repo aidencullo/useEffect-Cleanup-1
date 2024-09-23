a389de7da4fef70fca36d841945658c161c84cdb
Aiden Cullo <aidencullo@gmail.com>
2024-09-23 16:19:45 -0400

    Example 5 of 5: Tracking element visibility

--- src/App.js
+++ src/App.js
@@ -1,20 +1,21 @@
-import { useState } from 'react';
-import ModalDialog from './ModalDialog.js';
+import Box from './Box.js';
 
 export default function App() {
-  const [show, setShow] = useState(false);
   return (
     <>
-      <button onClick={() => setShow(true)}>
-        Open dialog
-      </button>
-      <ModalDialog isOpen={show}>
-        Hello there!
-        <br />
-        <button onClick={() => {
-          setShow(false);
-        }}>Close</button>
-      </ModalDialog>
+      <LongSection />
+      <Box />
+      <LongSection />
+      <Box />
+      <LongSection />
     </>
   );
+}
+
+function LongSection() {
+  const items = [];
+  for (let i = 0; i < 50; i++) {
+    items.push(<li key={i}>Item #{i} (keep scrolling)</li>);
+  }
+  return <ul>{items}</ul>
 }
